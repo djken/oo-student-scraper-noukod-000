c1312368163eb9d4c45ba2788e1348424ce7b0b4
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-30 14:20:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,9 +30,9 @@ class Scraper
 
     # student[:profile_quote] = page.css(".profile-quote")
     # student[:bio] = page.css("div.description-holder p")
-    container = page.css(".social-icon-container a").collect do |icon| 
+    container = page.css(".social-icon-container a").collect do |icon|
       icon.attribute("href").value
-      
+
     container.each do |link|
       if link.include?("twitter")
         student[:twitter] = link
