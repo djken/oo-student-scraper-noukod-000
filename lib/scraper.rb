dfff1ab4e56f09c51113a842c733c1fdd80598ab
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-30 13:23:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,11 @@ class Scraper
 
   # this method is responsible for scraping the index page that lists all of the students
   def self.scrape_index_page(index_url)
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
