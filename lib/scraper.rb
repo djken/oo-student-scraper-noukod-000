88b771f1052a8c4ca044f35176ff105660fac22a
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-30 13:31:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,20 @@ class Scraper
 
   # this method is responsible for scraping the index page that lists all of the students
   def self.scrape_index_page(index_url)
+    def self.scrape_index_page(index_url)
+    page = Nokogiri::HTML(open(index_url))
+    students = []
 
+    page.css("div.student-card").each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attribute("href").value
+      student_info = {:name => name,
+                :location => location,
+                :profile_url => profile_url}
+      students << student_info
+      end
+    students
   end
 
   # This method is responsible for scraping an individual student's profile page
